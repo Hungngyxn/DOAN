9967852f262dae223f9e634c3028af20c8b5d89e
Hungngyxn <100173529+Hungngyxn@users.noreply.github.com>
2022-11-11 14:01:44 +0700

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('HOST', 'sql310.epizy.com');
-    define('USER', 'epiz_32868731');
-    define('PASSWORD','aVoFqZEOqQNtR');
-    define('DB','epiz_32868731_diorweb');
+    define('HOST', '');
+    define('USER', '');
+    define('PASSWORD','');
+    define('DB','');
